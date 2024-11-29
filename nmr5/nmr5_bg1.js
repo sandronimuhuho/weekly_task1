240acaf0abc684571fbe7cfc74f82677bba751e6
sandro nimunuho <sandronimunuo@gmail.com>
2024-11-29 16:51:10 +0800

    tugas nomor lima

---
+++ nmr5/nmr5_bg1.js
@@ -0,0 +1,5 @@
+const arr1 = [1, 2,3];
+const arr2 = [4, 5, 6];
+
+const newarr = [...arr1, ...arr2];
+console.log(newarr);
---
+++ nmr5/nmr5_bg2.js
@@ -0,0 +1,8 @@
+const person = {
+    name : "Jhon",
+    age : "30"
+
+};
+
+const newperson ={ ...person, gemder : "male"};
+console.log(newperson);
