7270d65cbb99dd2d63d4e79fa6d7f5896840da8a
sandro nimunuho <sandronimunuo@gmail.com>
2024-11-29 16:49:21 +0800

    tugas nomor dua

---
+++ nmr2.js
@@ -0,0 +1,19 @@
+const product = {
+    name : "nimunuho",
+    rating : 4, 
+    price : 50000,
+    isActivasi : true,
+    images : ["img1.jpg", "img2.jpg", "img3.jpg", "img4.jpg" ," img5.jpg", "img6.jpg", "img7.jpg" ],
+    description : "kemeja pria lengan panjang slimfit kemeja kerja",
+    varians : {
+        name : "warna ",
+        options : ["Hitam", "Navy", "Army","Grey", "Coksu", "Maroon", "Putih"],
+    },
+    size : { 
+        name : "size ",
+        ukuran : [ "M", "L", "xl", "XXL"],
+    },
+    storeName : "nimunuho shop",
+
+};
+console.log(product);
